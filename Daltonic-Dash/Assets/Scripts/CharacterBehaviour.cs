64ab25701a31903274fdd93121c46ad5d9720ccd
tomas <tomas.vicente.7e5@itb.cat>
2024-11-07 15:57:06 +0100

    camera focused on player

---
+++ Daltonic-Dash/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] private float jumpForce = 10f;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Jump();
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
