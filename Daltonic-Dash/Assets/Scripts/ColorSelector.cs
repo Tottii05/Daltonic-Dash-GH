0eccb5f4b1833d95b1829982b83f89e549819f49
Joaquin Tech Tips <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-11-15 16:26:06 +0100

    Added color selection
    Añadido canvas de selección de color
    Añadido el evento que hace toggle entre ese canvas y el canvas inicial
    Añadido evento que hace set de los colores de pinchos que se usarán (a la espera de saber si también hay que pasar el color de fondo)
    Comprobado el funcionamiento del uso de PlayerPrefs

--- Daltonic-Dash/Assets/Scripts/ColorSelector.cs
+++ Daltonic-Dash/Assets/Scripts/ColorSelector.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class ColorSelector : MonoBehaviour
+public class ColorSelector1 : MonoBehaviour
 {
-    public void SetColorPalette(Color color1, Color color2)
-    { 
-        PlayerPrefs.SetFloat("Color1_R", color1.r);
-        PlayerPrefs.SetFloat("Color1_G", color1.g);
-        PlayerPrefs.SetFloat("Color1_B", color1.b);
-
-        PlayerPrefs.SetFloat("Color2_R", color2.r);
-        PlayerPrefs.SetFloat("Color2_G", color2.g);
-        PlayerPrefs.SetFloat("Color2_B", color2.b);
-
-        PlayerPrefs.Save();
+    
+    public void SelectDeutreranopy()
+    {
+        PlayerPrefs.SetString("BGColor", "#D9534F");
+        PlayerPrefs.SetString("SpykeColor", "#5CB85C");
     }
+    
+    public void SelectTritanopy()
+    {
+        PlayerPrefs.SetString("SpykeColor", "#59E59A");
+    }
+
+    public void SelectProtanopy()
+    {
+        PlayerPrefs.SetString("SpykeColor", "#C9302C");
+    } 
 }
--- Daltonic-Dash/Assets/Scripts/EventSystemMainMenu.cs
+++ Daltonic-Dash/Assets/Scripts/EventSystemMainMenu.cs
@@ -1,18 +1,21 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EventSystemMainMenu : MonoBehaviour
 {
-    private bool settingsController = false;
+    private bool settingsController;
+    private bool colorsController = false;
     private GameObject Canvas;
     private GameObject settingsCanvas;
+    private GameObject colorsCanvas;
     public void Start()
     {
         settingsController = false;
         Canvas = GameObject.Find("Canvas");
         settingsCanvas = GameObject.Find("SettingsCanvas");
+        colorsCanvas = GameObject.Find("Canva_DaltonicType");
         settingsCanvas.SetActive(false);
+        colorsCanvas.SetActive(false);
     }
 
 
@@ -23,15 +26,18 @@ public class EventSystemMainMenu : MonoBehaviour
     }
 
     public void endGame() {
-        //EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
-    public void openSettings()
+    public void toggleSettings()
     {
         settingsController = !settingsController;
-        settingsCanvas.SetActive(!settingsController);
-        settingsCanvas.SetActive(settingsController);
-        
+        settingsCanvas.SetActive(settingsController);        
+    }
+
+    public void toggleColors()
+    {
+        colorsController = !colorsController;
+        colorsCanvas.SetActive(colorsController);
     }
 }
