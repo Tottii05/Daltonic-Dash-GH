0b6edac0a6556fc49c08496c590d9582c4485aa4
Joaquin Tech Tips <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-11-19 15:22:35 +0100

    Color selector changes
    -Color selection functions moved to EventSystemMainMenu script for more compact code
    -Deleted Color Selector 1 script

--- Daltonic-Dash/Assets/Scripts/EventSystemMainMenu.cs
+++ Daltonic-Dash/Assets/Scripts/EventSystemMainMenu.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EventSystemMainMenu : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class EventSystemMainMenu : MonoBehaviour
     private GameObject Canvas;
     private GameObject settingsCanvas;
     private GameObject colorsCanvas;
+    private GameObject volumeSlider;
     public void Start()
     {
         settingsController = false;
@@ -16,28 +18,61 @@ public class EventSystemMainMenu : MonoBehaviour
         colorsCanvas = GameObject.Find("Canva_DaltonicType");
         settingsCanvas.SetActive(false);
         colorsCanvas.SetActive(false);
+
+        volumeSlider = GameObject.Find("VolumeSetting");
     }
 
 
-    //For button presses
+    ///For button presses
+    //Start button
     public void startGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    //Quit button
     public void endGame() {
         Application.Quit();
     }
 
+    //Settings button
     public void toggleSettings()
     {
         settingsController = !settingsController;
-        settingsCanvas.SetActive(settingsController);        
+        settingsCanvas.SetActive(settingsController);
+        Canvas.SetActive(!settingsController);
     }
 
+    //Color menu button
     public void toggleColors()
     {
         colorsController = !colorsController;
         colorsCanvas.SetActive(colorsController);
     }
+
+    //Color change buttons in the ColorMenu
+    public void SelectDeutreranopy()
+    {
+        PlayerPrefs.SetString("BGColor", "#D9534F");
+        PlayerPrefs.SetString("SpykeColor", "#5CB85C");
+        Debug.Log("esto funcha");
+    }
+
+    public void SelectTritanopy()
+    { 
+        PlayerPrefs.SetString("SpykeColor", "#59E59A");
+    }
+
+    public void SelectProtanopy()
+    {
+        PlayerPrefs.SetString("SpykeColor", "#C9302C");
+    }
+
+    ///Sliders
+    //For volume slider
+    public void setVolume()
+    {
+        PlayerPrefs.SetFloat("Volume", volumeSlider.GetComponent<Slider>().value);
+    }
+
 }
