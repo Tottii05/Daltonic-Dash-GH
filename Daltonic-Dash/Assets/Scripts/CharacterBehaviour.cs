453d60b051964edb8e88e44b55065a6afc6b58c2
Joaquin Tech Tips <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-11-27 17:39:10 +0100

    Spikes hotfix
    Spike hotfix so it works
    Github icon also works now :p

--- Daltonic-Dash/Assets/Scripts/CharacterBehaviour.cs
+++ Daltonic-Dash/Assets/Scripts/CharacterBehaviour.cs
@@ -56,7 +56,7 @@ public class CharacterBehaviour : MonoBehaviour
         }
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Spyke")
         {
